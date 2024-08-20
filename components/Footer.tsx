1035a3e00cc2fe8366e7e9baff29eaa1e76c79fc
nicobustelo <nbustelo2002@gmail.com>
2024-08-20 14:28:06 -0300

    Traduccion de texto a español

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-ts text-black">
-                    &copy; 2024 Store, Inc. All rights reserved.
+                    &copy; 2024 Molina Store. Todos los derechos reservados.
                 </p>
             </div>
         </footer>
